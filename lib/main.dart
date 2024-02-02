08f72c76fbfa45234d68a14e1f7a2e4a98552b18
Alexander Ma <maalexander2@yahoo.com>
2024-02-01 18:40:39 -0800

    Set up initial Firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,14 @@ import 'screens/profile.dart';
 import 'screens/home_page.dart';
 import 'screens/gacha.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const TaskPals());
 }
 
