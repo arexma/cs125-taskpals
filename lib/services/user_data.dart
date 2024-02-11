d50d78f26c0882f98958ccf07dbf3a7ee577f2f2
Alexander Ma <maalexander2@yahoo.com>
2024-02-11 06:01:33 -0800

    Added package for checking device id

--- lib/services/user_data.dart
+++ lib/services/user_data.dart
@@ -10,7 +10,7 @@
 //    tasks --> [string] (task descriptions)
 
 // TODO
-//  Error handling for async functions
+//  Error handling
 
 import 'dart:convert';
 import 'dart:async';
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
+import device_info_plus
 import firebase_core
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
