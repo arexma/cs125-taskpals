d6aa608459565313b4d76209e8c86ea5e169f953
Alexander Ma <maalexander2@yahoo.com>
2024-02-13 12:44:33 -0800

    dasjkldjsa

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
-import 'screens/profile.dart';
-import 'screens/home_page.dart';
-import 'screens/tasks.dart';
 import 'screens/gacha.dart';
-import 'services/user_data.dart';
+import 'screens/home_page.dart';
 import 'screens/first_time_user.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:android_id/android_id.dart';
+import 'services/user_data.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,19 +21,76 @@ void main() async {
   );
 }
 
-class TaskPals extends StatelessWidget {
+class TaskPals extends StatefulWidget {
   const TaskPals({Key? key}) : super(key: key);
 
-  Future<String?> _getDeviceID(BuildContext context) async {
-    var deviceInfo = DeviceInfoPlugin();
+  @override
+  State<TaskPals> createState() => _TaskPals();
+}
+
+class _TaskPals extends State<TaskPals> {
+  // Probably want to have global state management for database
+  // connection, deviceID, and maybe isFirstTimeUser (?)
+  bool? isFirstTimeUser;
+  late UserDataFirebase user;
+  late String deviceID;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> _getDeviceID(BuildContext context) async {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    String? id;
     if (Theme.of(context).platform == TargetPlatform.android) {
       const androidIdPlugin = AndroidId();
-      return await androidIdPlugin.getId();
+      id = await androidIdPlugin.getId();
     } else if (Theme.of(context).platform == TargetPlatform.iOS) {
       IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
-      return iosInfo.identifierForVendor;
+      id = iosInfo.identifierForVendor;
     }
+
+    setState(() {
+      if (id == null) {
+        // Error handling if we can't extract device ID
+      } else {
+        deviceID = id;
+      }
+    });
   }
+
+  Future<void> firstTimeUser(BuildContext context) async {
+    await _getDeviceID(context);
+    user = UserDataFirebase(deviceID);
+    await user.initializationComplete();
+    setState(() {
+      isFirstTimeUser = user.isEmpty() ? true : false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Pals',
+      home: FutureBuilder<void>(
+        future: firstTimeUser(context),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return isFirstTimeUser! ? const FirstTimeUser() : const HomePage();
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      ),
+    );
+  }
+}
+
+
+/*
+class TaskPals extends StatelessWidget {
+  const TaskPals({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -43,35 +98,6 @@ class TaskPals extends StatelessWidget {
       '/first': (context) => const FirstTimeUser(),
     });
   }
-
-  /*
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(title: 'Task Pals', initialRoute: '/tasks', routes: {
-      '/tasks': (context) => const TasksPageStarter(),
-    });
-  }
-  */
-
-  /*
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Task Pals',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.amber,
-        ), // Customize global color theming
-        textTheme: const TextTheme(), // Customize global text theming
-      ),
-      initialRoute: '/profile',
-      routes: {
-        '/profile': (context) => const ProfileScreen(),
-      },
-    );
-  }
-  */
 
   /*
   @override
@@ -92,3 +118,4 @@ class TaskPals extends StatelessWidget {
     );
   */
 }
+*/
