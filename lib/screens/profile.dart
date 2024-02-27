f3ae2016191f99e9c4e935de8b8add0eafbdf768
Alexander Ma <arma1@uci.edu>
2024-02-26 20:08:16 -0800

    Modified background color, added some new fields, made currency and pals collected uneditable, box sizing based on context size

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -3,21 +3,17 @@ User-editable:
 name
 height
 weight
+age
 
 Non-user-editable:
-
 user data with healthkit stuff
 calendar with progress done per day?
 tasks done
 pals collected (owned/possible)
-height/weight
 */
 
 // TODO:
 // Maybe remove box around text unless the user is updating it
-// Add task calendar
-// Add user data w/ health-kit stuff
-// Make pals collected not user editable
 // Create userInfo map based on stored user data
 // Maybe move screen up if the keyboard covers wherever the user is editing?
 // Editable profile picture
@@ -27,11 +23,13 @@ import '../utility/editable_field.dart';
 
 import '../services/user_data.dart';
 
-const Map<String, String> userInfo = {
-  'Name': 'Alexander Rex Ma',
-  'Height': '5\' 8"',
-  'Weight': '135',
-  'Pals Collected': '4/20',
+const Map<String, String> fields = {
+  'name': 'Name',
+  'height': 'Height (inches)',
+  'weight': 'Weight (lbs)',
+  'age': 'Age',
+  'currency': 'Currency',
+  'pals_collected': 'Pals Collected',
 };
 
 class ProfileScreen extends StatelessWidget {
@@ -45,51 +43,75 @@ class ProfileScreen extends StatelessWidget {
         preferredSize: const Size.fromHeight(25.0),
         child: AppBar(),
       ),
-      body: Column(
-        children: <Widget>[
-          const Center(
-            child: Text(
-              'Profile',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 45.0,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const Center(
+              child: Text(
+                'Profile',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 45.0,
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            width: 200.0,
-            height: 200.0,
-            child: ClipOval(
-              child: Image.asset(
-                ('lib/assets/pfp.jpg'),
-                fit: BoxFit.cover,
+            SizedBox(
+              width: 200.0,
+              height: 200.0,
+              child: ClipOval(
+                child: Image.asset(
+                  ('lib/assets/pfp.jpg'),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          Column(
-            children: userInfo.entries
-                .map((entry) => buildColumn(entry.key, entry.value))
-                .toList(),
-          ),
-        ],
+            Column(
+              children: fields.entries
+                  .map(
+                    (entry) => buildColumn(
+                      entry.value,
+                      user.queryByUniqueID([entry.key])[entry.key],
+                      context,
+                      (dynamic value) {
+                        user.updateDatabase({entry.key: value});
+                      },
+                    ),
+                  )
+                  .toList(),
+            ),
+          ],
+        ),
       ),
-      backgroundColor: Colors.amber[100],
+      backgroundColor: Colors.lightBlue[100],
     );
   }
 }
 
-Widget buildColumn(String label, String initialText) {
+Widget buildColumn(String label, dynamic initialText, BuildContext context,
+    Function(dynamic) callback) {
   return Padding(
     padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
     child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         Text(
           label,
           style: const TextStyle(fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 4.0),
-        EditableTextField(initialText: initialText),
+        label == 'Pals Collected' || label == 'Currency'
+            ? Text(
+                label == 'Pals Collected'
+                    ? '${List<int>.from(initialText).length} / 151'
+                    : initialText.toString(),
+              )
+            : EditableTextField(
+                initialText: initialText.toString(),
+                boxWidth: MediaQuery.of(context).size.width * 0.8,
+                boxHeight: MediaQuery.of(context).size.height * 0.05,
+                textAlignment: Alignment.center,
+                callback: callback,
+              ),
       ],
     ),
   );
